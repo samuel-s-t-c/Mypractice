25aca582f0958df5b4e0e8dca9f5209c0365c714
Samuel.s.t <sheeplostop@gmail.com>
2019-10-30 23:07:54 +0800

    csapp: chapter 4

---
+++ csapp/cp4/main.s
@@ -0,0 +1,35 @@
+	.file	"main.c"
+	.text
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB23:
+	.cfi_startproc
+	pushq	%rbx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 3, -16
+	movl	$3, %ebx
+	testl	%ebx, %ebx
+	jmp	.L2
+.L3:
+	movl	%ebx, %edx
+	leaq	.LC0(%rip), %rsi
+	movl	$1, %edi
+	movl	$0, %eax
+	call	__printf_chk@PLT
+	subl	$1, %ebx
+.L2:
+	jne	.L3
+	movl	$0, %eax
+	popq	%rbx
+	.cfi_def_cfa_offset 8
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
