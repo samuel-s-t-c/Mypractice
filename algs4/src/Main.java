6f80bc0f18b88f0b55d78e1d43518dfcb211bd82
Samuel.s.t <sheeplostop@gmail.com>
2019-12-19 23:13:22 +0800

    algs4: code for learning

--- algs4/src/Main.java
+++ algs4/src/Main.java
@@ -1,15 +1,35 @@
-import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.*;
+
+import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	int N = Integer.parseInt(args[0]);
-	int lo = Integer.parseInt(args[1]);
-	int hi = Integer.parseInt(args[2]);
-	for (int i = 0; i < N; i++) {
-	    int temp = (int) StdRandom.uniform(lo, hi);
-        StdOut.printf("%2d\n", temp);
+    	double xlo = Double.parseDouble(args[0]);
+		double xhi = Double.parseDouble(args[1]);
+		double ylo = Double.parseDouble(args[2]);
+		double yhi = Double.parseDouble(args[3]);
+		int T = Integer.parseInt(args[4]);
+
+		StdOut.println(xlo);
+		Interval1D x = new Interval1D(xlo, xhi);
+		Interval1D y = new Interval1D(ylo, yhi);
+		Interval2D box = new Interval2D(x, y);
+		box.draw();
+
+		Counter c = new Counter("hits");
+		for (int t = 0; t < T; t++)
+		{
+			double X = Math.random();
+			double Y = Math.random();
+			Point2D p = new Point2D(X,Y);
+			if (box.contains(p)) c.increment();
+			else p.draw();
+		}
+		StdOut.println(c);
+		StdOut.println(box.area());
+
     }
-    }
+
 }
+
