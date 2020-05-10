00392015fb5634ac84aa1a7f5b53d63109ef7cb2
Samuel.s.t <sheeplostop@gmail.com>
2020-05-10 20:41:06 +0800

    shell

--- shellLearning/echo.sh
+++ shellLearning/echo.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-str='http://blog.seetiny.com';
-pattern='/^/';
-echo $str;
+func1()
+{
+	local var='func1 local'
+	func2
+}
+
+func2()
+{
+	unset var
+	echo "In fun2, var = $var"
+}
+
+func1
+declare -F
